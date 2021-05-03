a638b4a87d41b4f3212f24059cd2b72e9b41cc41
masiv396 <itmasivian@gmail.com>
2021-05-03 15:59:34 -0500

    Release 0.1.3

--- Sources/MasivianPush/MasivianPush.swift
+++ Sources/MasivianPush/MasivianPush.swift
@@ -69,6 +69,37 @@ public func sentToken(platformName: String,token: String,appId: String, auth: St
     
 }
 
+public func sentDisableToken(token: String, auth: String){
+    let json: [String: Any] = ["token": token]
+
+    let jsonData = try? JSONSerialization.data(withJSONObject: json)
+
+    // create post request
+    let url = URL(string: "https://push-prodtemp.masivapp.com/v1/notification/instanceRegistryCancel")!
+    var request = URLRequest(url: url)
+    request.setValue(auth, forHTTPHeaderField: "Authorization")
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
+    request.httpMethod = "POST"
+
+    // insert json data to the request
+    request.httpBody = jsonData
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let responseJSON = responseJSON as? [String: Any] {
+            print(responseJSON)
+        }
+    }
+
+    task.resume()
+    
+}
+
 public func sentLocation(messageId: String,country: String,countryCode: String,city: String, auth: String){
     let json: [String: Any] = ["id": messageId,
                                "country": country,
