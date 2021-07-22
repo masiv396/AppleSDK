e76f7bca9ff3d3e12e1f8cf26658a613b798ff48
Michael Fabian Montes Salazar <maicol_4346@hotmail.com>
2021-07-22 14:03:44 -0500

    28655: Sincronizar branch con release 0.1.4

--- Sources/MasivianPush/MasivianPush.swift
+++ Sources/MasivianPush/MasivianPush.swift
@@ -4,18 +4,18 @@ struct MasivianPush {
     var text = "Hello, World!"
 }
 
-public func sentState(messageId: String,state: String, auth: String) -> String {
-    let json: [String: Any] = ["Id": messageId,
+public func sentState(messageId: String,state: String, auth: String){
+    let json: [String: Any] = ["id": messageId,
                                "state": state]
 
     let jsonData = try? JSONSerialization.data(withJSONObject: json)
 
     // create post request
-    let url = URL(string: "http://LB-LabsMasivianPush-Sender-59007382.us-east-1.elb.amazonaws.com/v1/notificationState/update")!
+    let url = URL(string: "https://push-prodtemp.masivapp.com/v1/notificationState/update")!
     var request = URLRequest(url: url)
     request.setValue(auth, forHTTPHeaderField: "Authorization")
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("Accept", forHTTPHeaderField: "Content-Type")
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
     request.httpMethod = "POST"
 
     // insert json data to the request
@@ -33,7 +33,103 @@ public func sentState(messageId: String,state: String, auth: String) -> String {
     }
 
     task.resume()
-    let response = task.response?.description
-    return response!
+    
+}
+
+public func sentToken(platformName: String,token: String,appId: String, auth: String){
+    let json: [String: Any] = ["platformName": platformName,
+                               "token": token,
+                               "appId":appId]
+
+    let jsonData = try? JSONSerialization.data(withJSONObject: json)
+
+    // create post request
+    let url = URL(string: "https://push-prodtemp.masivapp.com/v1/association/registerToken")!
+    var request = URLRequest(url: url)
+    request.setValue(auth, forHTTPHeaderField: "Authorization")
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
+    request.httpMethod = "POST"
+
+    // insert json data to the request
+    request.httpBody = jsonData
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let responseJSON = responseJSON as? [String: Any] {
+            print(responseJSON)
+        }
+    }
+
+    task.resume()
+    
+}
+
+public func sentDisableToken(token: String, auth: String){
+    let json: [String: Any] = ["token": token]
+
+    let jsonData = try? JSONSerialization.data(withJSONObject: json)
+
+    // create post request
+    let url = URL(string: "https://push-prodtemp.masivapp.com/v1/notification/instanceRegistryCancel")!
+    var request = URLRequest(url: url)
+    request.setValue(auth, forHTTPHeaderField: "Authorization")
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
+    request.httpMethod = "POST"
+
+    // insert json data to the request
+    request.httpBody = jsonData
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let responseJSON = responseJSON as? [String: Any] {
+            print(responseJSON)
+        }
+    }
+
+    task.resume()
+    
+}
+
+public func sentLocation(messageId: String,country: String,countryCode: String,city: String, auth: String){
+    let json: [String: Any] = ["id": messageId,
+                               "country": country,
+                               "countryCode":countryCode,
+                               "city":city]
+
+    let jsonData = try? JSONSerialization.data(withJSONObject: json)
+
+    // create post request
+    let url = URL(string: "https://push-prodtemp.masivapp.com/v1/notificationState/updateLocation")!
+    var request = URLRequest(url: url)
+    request.setValue(auth, forHTTPHeaderField: "Authorization")
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.setValue("application/json", forHTTPHeaderField: "Accept")
+    request.httpMethod = "POST"
+
+    // insert json data to the request
+    request.httpBody = jsonData
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let responseJSON = responseJSON as? [String: Any] {
+            print(responseJSON)
+        }
+    }
+
+    task.resume()
     
 }
